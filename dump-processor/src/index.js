e681ad9d8e9496f600a506d53ea9c4f5acc25ec0
Mischa Rodermond <947626+mrpapercut@users.noreply.github.com>
2021-11-21 16:59:30 +0100

    Updated dump-processor

--- dump-processor/src/index.js
+++ dump-processor/src/index.js
@@ -103,6 +103,7 @@ class DumpsProcessor {
     extractFile(filename) {
         this.debug('log', `Extracting file ${filename}.json.gz`);
         child_process.spawnSync('gunzip', ['-k', `/app/dumps/${filename}.json.gz`]);
+        this.debug('log', `File extraction complete`);
     }
 
     readFile(dumpConfig) {
@@ -147,7 +148,4 @@ const systemsWithCoordinates7daysConfig = dumpsConfig[1];
 const systemsWithoutCoordinatesConfig = dumpsConfig[2];
 
 const processor = new DumpsProcessor(DEBUG);
-
-[systemsWithCoordinatesConfig, systemsWithCoordinates7daysConfig, systemsWithoutCoordinatesConfig].forEach(config => {
-    await processor.process(config);
-});
+processor.process(systemsWithCoordinatesConfig);
