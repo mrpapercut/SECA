4f311316dfeca4a0b9319959e214ae5815577a8c
Mischa Rodermond <m.rodermond@nfir.nl>
2025-01-11 15:34:46 +0100

    Read NavRoute on startup

--- frontend/journal/server.js
+++ frontend/journal/server.js
@@ -9,16 +9,22 @@ const port = 3001;
 const navrouteFilePath = path.join(process.env.USERPROFILE, './Saved Games/Frontier Developments/Elite Dangerous', './NavRoute.json');
 let currentRoute = {}
 
+function readNavRoute() {
+    const contents = fs.readFileSync(navrouteFilePath, { encoding: 'utf8' });
+
+    try {
+        const route = JSON.parse(contents);
+        currentRoute = route;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+readNavRoute();
+
 fs.watch(navrouteFilePath, (event, filename) => {
     if (event === 'change') {
-        const contents = fs.readFileSync(navrouteFilePath, { encoding: 'utf8' });
-
-        try {
-            const route = JSON.parse(contents);
-            currentRoute = route;
-        } catch (err) {
-            console.log(err);
-        }
+        readNavRoute();
     }
 });
 
