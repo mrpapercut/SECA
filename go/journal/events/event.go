cff5b61d25a46672f5104e0d60a4dd868a5c82d4
Mischa Rodermond <m.rodermond@nfir.nl>
2025-02-11 19:21:45 +0100

    Remove eventhandler log

--- go/journal/events/event.go
+++ go/journal/events/event.go
@@ -73,110 +73,57 @@ func ParseEvent[T any](rawEvent string) (T, error) {
 	return t, nil
 }
 
-type EventHandlerLog struct {
-	Commander                int64
-	LoadGame                 int64
-	Location                 int64
-	Statistics               int64
-	FSDJump                  int64
-	FSDTarget                int64
-	Scan                     int64
-	FSSBodySignals           int64
-	SAASignalsFound          int64
-	SAAScanComplete          int64
-	ScanOrganic              int64
-	MultiSellExplorationData int64
-	SellOrganicData          int64
-	Died                     int64
-	NavRoute                 int64
-	Docked                   int64
-	Undocked                 int64
-	Embark                   int64
-	Disembark                int64
-	Touchdown                int64
-	Liftoff                  int64
-	ApproachBody             int64
-	LeaveBody                int64
-	FSSSignalDiscovered      int64
-}
-
-var EHL = EventHandlerLog{}
-
 type EventHandler struct{}
 
 func (eh *EventHandler) HandleEvent(eventType string, rawEvent string) error {
 	switch eventType {
 	case "Commander":
-		EHL.Commander++
 		return eh.handleEventCommander(rawEvent)
 	case "LoadGame":
-		EHL.LoadGame++
 		return eh.handleEventLoadGame(rawEvent)
 	case "Location":
-		EHL.Location++
 		return eh.handleEventLocation(rawEvent)
 	case "Statistics":
-		EHL.Statistics++
 		return eh.handleEventStatistics(rawEvent)
 	case "FSDJump":
-		EHL.FSDJump++
 		return eh.handleEventFSDJump(rawEvent)
 	case "FSDTarget":
-		EHL.FSDTarget++
 		return eh.handleEventFSDTarget(rawEvent)
 	case "Scan":
-		EHL.Scan++
 		return eh.handleEventScan(rawEvent)
 	case "FSSBodySignals":
-		EHL.FSSBodySignals++
 		return eh.handleEventFSSBodySignals(rawEvent)
 	case "SAASignalsFound":
-		EHL.SAASignalsFound++
 		return eh.handleEventFSSBodySignals(rawEvent)
 	case "SAAScanComplete":
-		EHL.SAAScanComplete++
 		return eh.handleEventSAAScanComplete(rawEvent)
 	case "ScanOrganic":
-		EHL.ScanOrganic++
 		return eh.handleEventScanOrganic(rawEvent)
 	case "MultiSellExplorationData":
-		EHL.MultiSellExplorationData++
 		return eh.handleEventMultiSellExplorationData(rawEvent)
 	case "SellOrganicData":
-		EHL.SellOrganicData++
 		return eh.handleEventSellOrganicData(rawEvent)
 	case "Died":
-		EHL.Died++
 		return eh.handleEventDied(rawEvent)
 	case "NavRoute":
-		EHL.NavRoute++
 		return eh.handleEventNavRoute(rawEvent)
 	case "Docked":
-		EHL.Docked++
 		return eh.handleEventDocked(rawEvent)
 	case "Undocked":
-		EHL.Undocked++
 		return eh.handleEventUndocked(rawEvent)
 	case "Embark":
-		EHL.Embark++
 		return eh.handleEventEmbark(rawEvent)
 	case "Disembark":
-		EHL.Disembark++
 		return eh.handleEventDisembark(rawEvent)
 	case "Touchdown":
-		EHL.Touchdown++
 		return eh.handleEventTouchdown(rawEvent)
 	case "Liftoff":
-		EHL.Liftoff++
 		return eh.handleEventLiftoff(rawEvent)
 	case "ApproachBody":
-		EHL.ApproachBody++
 		return eh.handleEventApproachBody(rawEvent)
 	case "LeaveBody":
-		EHL.LeaveBody++
 		return eh.handleEventLeaveBody(rawEvent)
 	case "FSSSignalDiscovered":
-		EHL.FSSSignalDiscovered++
 		return eh.handleEventFSSSignalDiscovered(rawEvent)
 	case "Loadout":
 		return eh.handleEventLoadout(rawEvent)
